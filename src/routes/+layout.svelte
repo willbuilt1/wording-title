5e51f1b304ef197203cfb8db4499aca87366531d
Will Foster <wfoster12@gmail.com>
2024-07-13 11:10:32 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  let { children }: { children: Snippet } = $props();
+</script>
+
+<body>
+  {@render children()}
+  <!-- <slot /> -->Hello
+</body>
+
+<style>
+  body {
+    font-family: "Arial", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+</style>
